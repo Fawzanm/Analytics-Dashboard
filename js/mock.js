f00268403b91c78b470c5cefacd87cec89d5fe52
fawzan <fawzanm@gmail.com>
2015-02-06 14:30:06 +0530

    Added new files with dummy data & function prototypes.

---
+++ js/mock.js
@@ -0,0 +1,86 @@
+/**
+ * Created by fawzan on 2/6/15.
+ */
+
+var dashboard = [
+    {'title':'Sample-0', group:'A', 'id':'1'},
+    {'title':'Sample-1', group:'A', 'id':'2'},
+    {'title':'Sample-2', group:'A', 'id':'3'},
+    {'title':'Sample-3', group:'B', 'id':'3'},
+    {'title':'Sample-4', group:'B', 'id':'3'},
+    {'title':'Sample-5', group:'C', 'id':'3'}
+];
+
+var dashboardPageMeta = [
+    {'id':'1', 'title':'tab-1'},
+    {'id':'2', 'title':'tab-2'},
+    {'id':'3', 'title':'tab-3'},
+    {'id':'4', 'title':'tab-4'}
+];
+
+var dashboardPageContent = [
+    {'type':'widget', 'externalUrl':null, 'widgets':[
+        {
+            "config" : {
+                "x" : "0",
+                "y" : "0",
+                "width" : "200",
+                "height" : "200"
+            },
+
+            "readOnly" : true,
+
+            "data" : {
+                //will be added
+            }
+        },
+        {
+
+            "config" : {
+                "x" : "0",
+                "y" : "0",
+                "width" : "200",
+                "height" : "200"
+            },
+
+            "readOnly" : false,
+
+            "data" : {
+                //will be added
+            }
+        }
+    ]}
+];
+
+var widgets = [
+    {
+        "config" : {
+            "x" : "0",
+            "y" : "0",
+            "width" : "200",
+            "height" : "200"
+        },
+
+        "readOnly" : true,
+
+        "data" : {
+            //will be added
+        }
+    },
+    {
+
+        "config" : {
+            "x" : "0",
+            "y" : "0",
+            "width" : "200",
+            "height" : "200"
+        },
+
+        "readOnly" : false,
+
+        "data" : {
+            //will be added
+        }
+    }
+];
+
